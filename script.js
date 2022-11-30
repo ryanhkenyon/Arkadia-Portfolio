7e47099d8d986e786e4a7e4580e8b307226a099c
ryanhkenyon <ryanhkenyon@gmail.com>
2022-11-30 17:26:15 -0500

    CSS Overhaul
    -formatted sections of the page, each with unique art designs and backgrounds

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-const navHome = document.getElementById('navHome');
-
-
-navHome.addEventListener('click',()=>{
-    document.getElementById('navHome').style.color = 'red';
-    // TODO: get it to change back to black
-});
