84dfa73d8375e4e86ee193004ec1e607fccf97a1
ryanhkenyon <ryanhkenyon@gmail.com>
2022-11-30 08:43:09 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const navHome = document.getElementById('navHome');
+
+
+navHome.addEventListener('click',()=>{
+    document.getElementById('navHome').style.color = 'red';
+    // TODO: get it to change back to black
+});
